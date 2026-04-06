18f7ad109b6fadb8c842a65b0136957dd3ad113c
Khương Phan <pn.ankhuong@gmail.com>
2026-04-06 13:41:04 +0700

    fix service.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
-import dotenv from 'dotenv';
 import { createServer } from 'http';
 import pool from './config/db.js';
 
